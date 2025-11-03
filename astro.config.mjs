dc324d8580c020281a2978c3280b41e3ab321c20
Daniel Garcia <github@mail.daniel.es>
2025-11-03 09:53:38 +0000

    Initial config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import rehypeExternalLinks from "rehype-external-links";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    mdx({
+      rehypePlugins: [[rehypeExternalLinks, { target: "_blank", rel: [] }]],
+    }),
+    react(),
+    sitemap(),
+  ],
+  trailingSlash: "always",
+});
